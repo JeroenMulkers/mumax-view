a2b100784a9095a3acc7eae655b5abdfe80242f5
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2019-11-27 10:59:59 +0100

    restructure main file

--- main.cpp
+++ main.cpp
@@ -12,68 +12,68 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include "arrowmodel.hpp"
-#include "camera.hpp"
-#include "field.hpp"
 #include "fieldrenderer.hpp"
-#include "shaderprogram.hpp"
-#include "shaders.hpp"
-
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
 
 std::function<void()> loop;
 void main_loop() {
   loop();
 };
 
+// TODO: put mouse and window in a descent window class
+class Mouse {
+ public:
+  bool leftButtonDown;
+  bool middleButtonDown;
+  double lastX;
+  double lastY;
+};
+Mouse mouse;
 GLFWwindow* window;
+
 FieldRenderer* renderer;
 
 extern "C" {
-
 #ifdef __EMSCRIPTEN__
-void EMSCRIPTEN_KEEPALIVE updateArrowShaftRadius(float r) {
+EMSCRIPTEN_KEEPALIVE
+void updateArrowShaftRadius(float r) {
   renderer->arrow.setShaftRadius(r);
   renderer->needRender = true;
 }
-
-void EMSCRIPTEN_KEEPALIVE updateArrowHeadRadius(float r) {
+EMSCRIPTEN_KEEPALIVE
+void updateArrowHeadRadius(float r) {
   renderer->arrow.setHeadRadius(r);
   renderer->needRender = true;
 }
-
-void EMSCRIPTEN_KEEPALIVE updateArrowHeadRatio(float r) {
+EMSCRIPTEN_KEEPALIVE
+void updateArrowHeadRatio(float r) {
   renderer->arrow.setHeadRatio(r);
   renderer->needRender = true;
 }
-
-void EMSCRIPTEN_KEEPALIVE updateArrowSegments(float n) {
+EMSCRIPTEN_KEEPALIVE
+void updateArrowSegments(float n) {
   renderer->arrow.setSegments(n);
   renderer->needRender = true;
 }
-
-void EMSCRIPTEN_KEEPALIVE updateArrowScalingsFactor(float s) {
+EMSCRIPTEN_KEEPALIVE
+void updateArrowScalingsFactor(float s) {
   renderer->arrowScalingsFactor = s;
   renderer->needRender = true;
 }
-
-void EMSCRIPTEN_KEEPALIVE loadfile(std::string filename) {
+EMSCRIPTEN_KEEPALIVE
+void loadfile(std::string filename) {
   std::cout << filename << std::endl;
   Field* oldfield = renderer->field_;
   renderer->setField(readFieldFromOVF(filename));
   delete oldfield;
 }
-
 EM_JS(int, canvas_get_width, (), {
   return document.getElementById('canvas').scrollWidth;
 });
-
 EM_JS(int, canvas_get_height, (), {
   return document.getElementById('canvas').scrollHeight;
 });
-
-void EMSCRIPTEN_KEEPALIVE updateCanvasSize() {
+EMSCRIPTEN_KEEPALIVE
+void updateCanvasSize() {
   if (!window) {
     return;
   }
@@ -83,99 +83,33 @@ void EMSCRIPTEN_KEEPALIVE updateCanvasSize() {
   glViewport(0, 0, width, height);
   renderer->needRender = true;
 }
-
 #endif
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-  renderer->needRender = true;
-  glViewport(0, 0, width, height);
-}
-
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
-  renderer->needRender = true;
-  renderer->camera.targetDistance *= (1 - 0.1 * (float)yoffset);
-  // camera.position += (float)yoffset * 0.1f * (camera.target -
-  // camera.position);
-}
-
-class Mouse {
- public:
-  bool leftButtonDown;
-  bool middleButtonDown;
-  double lastX;
-  double lastY;
-};
-
-Mouse mouse;
-
-static void mouse_callback(GLFWwindow* window,
-                           int button,
-                           int action,
-                           int mods) {
-  if (button == GLFW_MOUSE_BUTTON_LEFT) {
-    if (GLFW_PRESS == action) {
-      mouse.leftButtonDown = true;
-      glfwGetCursorPos(window, &mouse.lastX, &mouse.lastY);
-    } else if (GLFW_RELEASE == action) {
-      mouse.leftButtonDown = false;
-    }
-  } else if (button == GLFW_MOUSE_BUTTON_MIDDLE) {
-    if (GLFW_PRESS == action) {
-      mouse.middleButtonDown = true;
-      glfwGetCursorPos(window, &mouse.lastX, &mouse.lastY);
-    } else if (GLFW_RELEASE == action) {
-      mouse.middleButtonDown = false;
-    }
-  }
-}
-
-void mousemove_callback(GLFWwindow* window, double xpos, double ypos) {
-  float xoffset = xpos - mouse.lastX;
-  float yoffset = mouse.lastY - ypos;
-  mouse.lastX = xpos;
-  mouse.lastY = ypos;
-
-  if (mouse.leftButtonDown) {
-    renderer->needRender = true;
-    float sensitivity = 0.02;
-    xoffset *= sensitivity;
-    yoffset *= sensitivity;
-
-    renderer->camera.yaw += xoffset;
-    renderer->camera.pitch -= yoffset;
-  }
-
-  if (mouse.middleButtonDown) {
-    renderer->needRender = true;
-    // todo: move camera
-    renderer->camera.target -= 0.05f * yoffset * renderer->camera.up();
-    // camera.target[0] -= 0.05 * xoffset;
-    // camera.target[2] -= 0.05 * yoffset;
-  }
-}
-
-static void error_callback(int error, const char* description) {
-  std::cerr << "Error: " << description << std::endl;
-}
+// ------- GLFW CALLBACKS ------------------------------------------------------
+void frameBufferSizeCallback(GLFWwindow* window, int width, int height);
+void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+void curserPosCallback(GLFWwindow* window, double xpos, double ypos);
+void errorCallback(int error, const char* description);
 
 int main() {
-  // ------- INIT GLFW ----------------------------------------
+  // ------- INIT GLFW ---------------------------------------------------------
 
-  glfwSetErrorCallback(error_callback);
+  glfwSetErrorCallback(errorCallback);
   if (!glfwInit()) {
     std::cerr << "glfwInit failed" << std::endl;
     return -1;
   }
 
-  // ------- CREATE WINDOW ----------------------------------------
+  // ------- CREATE WINDOW -----------------------------------------------------
 
 #ifdef __EMSCRIPTEN__
   int width = canvas_get_width();
   int height = canvas_get_height();
 #else
-  int width = SCR_WIDTH;
-  int height = SCR_HEIGHT;
+  int width = 800;
+  int height = 600;
 #endif
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -193,10 +127,10 @@ int main() {
   }
 
   glfwMakeContextCurrent(window);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-  glfwSetScrollCallback(window, scroll_callback);
-  glfwSetMouseButtonCallback(window, mouse_callback);
-  glfwSetCursorPosCallback(window, mousemove_callback);
+  glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+  glfwSetScrollCallback(window, scrollCallback);
+  glfwSetMouseButtonCallback(window, mouseButtonCallback);
+  glfwSetCursorPosCallback(window, curserPosCallback);
 
   // -------- GL SETTINGS --------------------------------------------------
 
@@ -231,7 +165,64 @@ int main() {
   //-------- CLEAN UP --------------------------------------------------------
 
   delete renderer;
-  glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
+}
+
+void frameBufferSizeCallback(GLFWwindow* window, int width, int height) {
+  renderer->needRender = true;
+  glViewport(0, 0, width, height);
+}
+
+void scrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
+  renderer->needRender = true;
+  renderer->camera.targetDistance *= (1 - 0.1 * (float)yoffset);
+  // camera.position += (float)yoffset * 0.1f * (camera.target -
+  // camera.position);
+}
+
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+  if (button == GLFW_MOUSE_BUTTON_LEFT) {
+    if (GLFW_PRESS == action) {
+      mouse.leftButtonDown = true;
+      glfwGetCursorPos(window, &mouse.lastX, &mouse.lastY);
+    } else if (GLFW_RELEASE == action) {
+      mouse.leftButtonDown = false;
+    }
+  } else if (button == GLFW_MOUSE_BUTTON_MIDDLE) {
+    if (GLFW_PRESS == action) {
+      mouse.middleButtonDown = true;
+      glfwGetCursorPos(window, &mouse.lastX, &mouse.lastY);
+    } else if (GLFW_RELEASE == action) {
+      mouse.middleButtonDown = false;
+    }
+  }
+}
+
+void curserPosCallback(GLFWwindow* window, double xpos, double ypos) {
+  float xoffset = xpos - mouse.lastX;
+  float yoffset = mouse.lastY - ypos;
+  mouse.lastX = xpos;
+  mouse.lastY = ypos;
+
+  if (mouse.leftButtonDown) {
+    renderer->needRender = true;
+    float sensitivity = 0.02;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    renderer->camera.yaw += xoffset;
+    renderer->camera.pitch -= yoffset;
+  }
+  // TODO: fix middle button feature
+  // if (mouse.middleButtonDown) {
+  //  renderer->needRender = true;
+  //  renderer->camera.target -= 0.05f * yoffset * renderer->camera.up();
+  //  // camera.target[0] -= 0.05 * xoffset;
+  //  // camera.target[2] -= 0.05 * yoffset;
+  //}
+}
+
+void errorCallback(int error, const char* description) {
+  std::cerr << "Error: " << description << std::endl;
 }
